760cb22dc0007a333bcc34300eed823463298886
HISHAM C <91016239+HISHAMCHOKLY@users.noreply.github.com>
2022-10-04 13:24:21 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -4,3 +4,8 @@ const app=express()
 app.get("*",(req,res)=>{
     res.send("<h1>HELLO GUYZz........</h1>")
 })
+
+port=process.env.PORT||5000
+app.listen(port,()=>{
+    console.log('app started on port '+port);
+})
